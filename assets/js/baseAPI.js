d0fe82c631d176b252bff2f77fa06cebeb92a975
zhou <z17333262851@163.com>
2022-07-12 10:47:21 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,8 +1,27 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
-    // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-    // console.log(options.url);
-  })
+$.ajaxPrefilter(function (options) {
+  // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
+  options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+  // console.log(options.url);
+  // 统一为有权限的接口,设置headers请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = { authorization: localStorage.getItem('token') || '' }
+  }
+
+  // 全局统一挂在 complete 回调函数
+  options.complete = function (res) {
+    // console.log('执行了 complete 回调：')
+    // console.log(res)
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 1. 强制清空 token
+      localStorage.removeItem('token')
+      // 2. 强制跳转到登录页面
+      location.href = '/login.html'
+    }
+
+
+  }
+
+})
---
+++ assets/js/index.js
@@ -0,0 +1,64 @@
+$(function () {
+    getUserInfo();
+    var layer = layui.layer
+
+    $('#btnLogOut').on('click', function () {
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // 1. 清空本地存储的token
+            localStorage.removeItem('token')
+            // 2. 跳转到登录页面
+            location.href = '/login.html'
+            // 关闭confirm询问框
+            layer.close(index);
+        });
+    })
+})
+
+//获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers 就是请求头配置对象
+        // headers:{
+        //     authorization:localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            // console.log(res);
+            if (res.status == 0) {
+                renderAvatar(res.data)
+            }
+        },
+        // complete: function (res) {
+        //     // console.log('执行了 complete 回调：')
+        //     // console.log(res)
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         // 1. 强制清空 token
+        //         localStorage.removeItem('token')
+        //         // 2. 强制跳转到登录页面
+        //         location.href = '/login.html'
+        //     }
+        // }
+    })
+}
+
+// 渲染用户的头像
+function renderAvatar(user) {
+    // 1.获取用户名称
+    var name = user.nickname || user.username
+    // 2. 设置欢迎文本
+    $('#welcome').html('欢迎&nbsp&nbsp' + name)
+    // 3.按需渲染用户头像
+    if (user.pic !== null) {
+        // 3.1渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show
+        $('.text-avatar').hide()
+        // 3.2渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+
+}
